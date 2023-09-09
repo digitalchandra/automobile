118378409a7a3de0cb3ea920a4802777318c2c4c
seoshrestha <chandra.shrestha@digitaltransformationsystem.com>
2023-09-09 18:02:44 +0545

    hopage

---
+++ Desktop/linosautomobile/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap'
+import MainHomepage from './homepage/MainHomepage';
+
+function App() {
+  return (
+    <div className="App">
+        <MainHomepage/>
+    </div>
+  );
+}
+
+export default App;
